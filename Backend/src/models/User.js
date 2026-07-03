0c1925a267a2918db9b8c84cfad1cdbd48344783
ujjwal soni <ujjwalsoni08@gmail.com>
2026-07-03 14:14:48 +0530

    Fix next is not a function error in User.js pre-save hook

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -50,11 +50,10 @@ const userSchema = new mongoose.Schema(
     }
 );
 
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password') || !this.password) return next();
+userSchema.pre('save', async function() {
+    if (!this.isModified('password') || !this.password) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 userSchema.methods.comparePassword = async function(enteredPassword) {
