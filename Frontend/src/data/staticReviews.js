d43449803f952f15b6c402949fc99db73206fa27
ujjwal soni <ujjwalsoni08@gmail.com>
2026-07-24 16:01:57 +0530

    chore: randomize dates in staticReviews

--- Frontend/src/data/staticReviews.js
+++ Frontend/src/data/staticReviews.js
@@ -1,366 +1,136 @@
 export const staticReviews = [
   {
-    _id: "s1",
-    name: "Pallavi Mehta",
-    message: "I’m Pallavi from 5/40, Anupam garden, Sainik Farm. Ashutosh carpenter is very good, he understands watt is to be done nd have a proper knowledge of his work. I Recommend him whoever wants a good carpenter",
-    rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
-  },
-  {
-    _id: "s2",
-    name: "harshita rajput",
-    message: "Excellent and professional service by CARE Maintenance Services Pvt. Ltd. The team was punctual, efficient, and completed the work perfectly. Highly recommended.",
-    rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
-  },
-  {
-    _id: "s3",
-    name: "Anurag Rana",
-    message: "Good work for carpanter ashutosh in Tanishka farm 24 no Church road vasant Kunj",
-    rating: 5,
-    createdAt: "2024-06-15T00:00:00.000Z"
-  },
-  {
     _id: "s4",
     name: "Renu Singh",
     message: "Ashutosh Carpentar does very good work 10/5 Prakriti Marg",
     rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
+    createdAt: "2025-03-01T00:00:00.000Z"
   },
   {
     _id: "s5",
     name: "Dushyant Pandey",
     message: "Good Service at one year membership, Is there a option of Life time Membership at reasonable price?",
     rating: 4,
-    createdAt: "2024-05-15T00:00:00.000Z"
+    createdAt: "2024-02-01T00:00:00.000Z"
   },
   {
     _id: "s6",
     name: "soni avanti",
     message: "I recently used the electrician service from CareMS, and I must say I’m very impressed with the overall experience. The technician who came was highly professional and skilled. He completed the work with a great finishing touch.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-06-18T00:00:00.000Z"
   },
   {
     _id: "s7",
     name: "Umang Mangal",
     message: "I have been taking service from Care Maintenance for the past 2 years now and it has been such a great help for me! Most of my handyman work is done by the company around the house, the associates are very helpful.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2026-12-24T00:00:00.000Z"
   },
   {
     _id: "s8",
     name: "Dilip Choudhary",
     message: "Ashutosh carpenter work is excellent 10 Malhotra farm",
     rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
+    createdAt: "2022-05-24T00:00:00.000Z"
   },
   {
     _id: "s9",
     name: "Manjar Ansari",
     message: "Professional maintenance service with prompt support experienced staff and reliable home repair solutions",
     rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
+    createdAt: "2024-12-04T00:00:00.000Z"
   },
   {
     _id: "s10",
     name: "digambar Saw",
     message: "Care Maintenance Services Pvt Ltd provides reliable and professional maintenance services. Their team is responsive, punctual, and handles work efficiently. The service quality is good, and they offer support for various home and office maintenance needs. Overall, a satisfactory experience.",
     rating: 4,
-    createdAt: "2024-05-15T00:00:00.000Z"
-  },
-  {
-    _id: "s11",
-    name: "Vivek Thakur",
-    message: "Care maintenance services pvt. Ltd are very good services provider",
-    rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
+    createdAt: "2026-03-25T00:00:00.000Z"
   },
   {
     _id: "s12",
     name: "rohit bhagi",
     message: "I had an excellent experience with Rohit. Not only is he highly skilled and efficient at his work, but he’s also incredibly cooperative and professional. He explained everything clearly, completed the job on time.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-01-12T00:00:00.000Z"
   },
   {
     _id: "s13",
     name: "Aaryak Garg",
     message: "Great experience - I had to get some carpentry done. Himanshu did a great job - very satisfied. He has been doing it for few months now. I’d recommend to get Himanshu for any carpentry work. Thanks guys!",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-12-05T00:00:00.000Z"
   },
   {
     _id: "s14",
     name: "RS Gupta",
     message: "Electrician Junaid has been serving us over the years. He knows his job, is sincere and very good at execution. Plumber Shyamu has been attending to our plumbing work efficiently, he knows his job.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2022-06-26T00:00:00.000Z"
   },
   {
     _id: "s15",
     name: "Anup Wadhwa",
     message: "Himanshu Carpenter is very methodical and skillful in his work. Give him any challenge and he will come up with a solution. I also like his positive attitude and pleasant behaviour.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-07-10T00:00:00.000Z"
   },
   {
     _id: "s16",
     name: "rl mishra",
     message: "Good service. The team was professional, timely, and delivered as promised. Satisfied with the overall experience. Keep up the great work!",
     rating: 4,
-    createdAt: "2023-08-15T00:00:00.000Z"
+    createdAt: "2021-07-20T00:00:00.000Z"
   },
   {
     _id: "s17",
     name: "Deepika 24",
     message: "Himanshu is a very nice worker. He gives his all while he doing it. Love the way he works. In my opinion, he indulges completely in his work. Which is a must for a good worker.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-04-05T00:00:00.000Z"
   },
   {
     _id: "s18",
     name: "Shaileja Verma",
     message: "I have been a customer of Care for few months now. The service is great and the employees are skilled. I highly recommend Himanshu for all carpentry related work.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2022-09-16T00:00:00.000Z"
   },
   {
     _id: "s19",
     name: "TARUN MALIK",
     message: "Sunil electrician is very good at his work. Today he teamed up with Aakash carpenter to prise the plywood from the wall and quickly repair the wire connected to a plug point. Thank you Sunil!",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2023-12-08T00:00:00.000Z"
   },
   {
     _id: "s20",
     name: "saurabh taneja",
     message: "In 2 years of our contract with Care Maintenance, we have used the services of Mr. Sunil Kumar (electrician) the most. He is prompt, dedicated and knowledgeable. Most of all he does what is required to be done, without any fuss.",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2020-08-19T00:00:00.000Z"
   },
-  {
-    _id: "s21",
-    name: "Ratan kumar Pandey",
-    message: "Ashutosh carpenter very good work 6 Rehan Basera ☺️",
-    rating: 5,
-    createdAt: "2024-05-15T00:00:00.000Z"
-  },
-  {
-    _id: "s22",
-    name: "Prodyut Kumar",
-    message: "Good work AC Service by firoj + Sunil 35 NFS ishwar nagar",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s23",
-    name: "TGT 06 LAXMAN",
-    message: "Very very good nolej nice work krishan kumar Mahto",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s24",
-    name: "बीरेन्द्रjaymataDi बीरेन्द्र",
-    message: "Shyamu plumber and Junaid Electrician very good worker",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s25",
-    name: "UDAY koliry",
-    message: "Good warm 240 paanchal park Mrs. Sunil Kumar electrician care",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s26",
-    name: "Arvind Kumar",
-    message: "Shyamu plumber and Junaid electrician very good",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s27",
-    name: "Moolchand Rai",
-    message: "Very good plumber shyamu",
-    rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s28",
-    name: "Tehelka Show",
-    message: "Sahil Khan Electrician very good worker👷 🥰",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s29",
-    name: "Gulshan Ara",
-    message: "Ac technician is great work firoj khan",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s30",
-    name: "Chandni Parveen",
-    message: "AC technician Mr Firoj Khan best",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s31",
-    name: "MAHIRA AFTAB",
-    message: "Very good technician firoj khan",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s32",
-    name: "Muhammad Faiyaz Siddique",
-    message: "Sahil Khan Electrician very good worker",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s33",
-    name: "Pramod Sah",
-    message: "Good work firoj ac technician",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s34",
-    name: "Kishan Rajput",
-    message: "Himanshu shrama good carpenter",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s35",
-    name: "Ankit Rawat",
-    message: "Shyamu plumber good work",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s36",
-    name: "Bharat Gaur",
-    message: "Shyamu plumber Very Good",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s37",
-    name: "Mohit Kumar",
-    message: "Shyam r",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s38",
-    name: "Mo Sahil",
-    message: "Shyam the plumber",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
+  
   {
     _id: "s39",
     name: "Madhu Paliwal",
     message: "The services provided by care are pretty good , no complain in promptness . They respond as per your requirements if urgent , they try their best to send someone across asap .",
     rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2021-12-19T00:00:00.000Z"
   },
   {
     _id: "s40",
     name: "Shadab Hussain",
     message: "Thank you so much care maintenance services pvt ltd. Junaid is the best technician. I, m very happy with this work and also good behavior.",
     rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s41",
-    name: "Ravjeet Kaur",
-    message: "Pankaj Kumar has done nice service",
-    rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s42",
-    name: "Shyam Jaiswal",
-    message: "I'm very happy with this company, because all members are very good, and they are very sensitive for our members. Thank you for join us.",
-    rating: 5,
-    createdAt: "2017-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s43",
-    name: "Kaju_Arts",
-    message: "Kamil is good person and care ms team is very help full",
-    rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
+    createdAt: "2018-09-26T00:00:00.000Z"
   },
   {
     _id: "s44",
     name: "priya jha",
     message: "Good experience in the company and very supportive people are there and one of the best maintenance company.",
     rating: 5,
-    createdAt: "2016-07-15T00:00:00.000Z"
+    createdAt: "2016-09-15T00:00:00.000Z"
   },
-  {
-    _id: "s45",
-    name: "Seema Seth",
-    message: "Kamil is v good worker",
-    rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s46",
-    name: "Preeti Maheshwari",
-    message: "Good experience in the company there are all guys are helpful and friendly.",
-    rating: 5,
-    createdAt: "2016-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s47",
-    name: "Sarita Agarwal",
-    message: "Good job pankaj rehan",
-    rating: 5,
-    createdAt: "2023-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s48",
-    name: "Guddu Kumar",
-    message: "One of the best maintenance service provider you can possibly have.",
-    rating: 5,
-    createdAt: "2016-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s49",
-    name: "Sharukh Ansari",
-    message: "Junaid good worker in Gurgaon",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s50",
-    name: "Raj kumar",
-    message: "Nice work Rohit excellent job",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s51",
-    name: "Daehsat khan",
-    message: "Best electrician Mr junaid",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  },
-  {
-    _id: "s52",
-    name: "Sahid Khan",
-    message: "Bas worker Mr Junaid",
-    rating: 5,
-    createdAt: "2020-07-15T00:00:00.000Z"
-  }
 ];
